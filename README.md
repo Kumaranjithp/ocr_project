e0c0fbe488143c1cc29729a7c4e84056a35c08ab
kumaranjithp <kumaranjithp@gmail.com>
2022-03-26 23:00:59 +0530

    dic python scrript updated

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ocr_project
 Machine Learning - OCR (Optical Character Recognisation) Project
+
+## Ranjith
+
+### Kumar
+
+'fdgdgfgh'
---
+++ dic.py
@@ -0,0 +1,10 @@
+back = {'one': "one", 2: "two", 3: "three", 4: "four"}
+print(back['one'])
+print(len(back))
+val = back.values() # values method to drag values as a list and saved in val variable
+print(val)
+print( 'two' in val)
+# lists search algorithm is linear search.(time gets longer in direct proportion to the lrn of the list
+# Dictionaries search algorithm is a hash table (hash func are so magical)
+
+
